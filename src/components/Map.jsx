7f00e0665f71037863511bb5de0e2ade2da5f9fd
DougO28 <douguiortegaq@gmail.com>
2026-01-20 09:46:53 -0600

    modificacion de icono de ubicacion

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Solución 1: Usar los iconos por defecto de Leaflet correctamente
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const position = [14.683456695656572, -91.0147648095703];
 
